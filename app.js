b168ef350d6e5b74e0094307a36f2bd967d413b2
chaemni0706 <kcm070611@naver.com>
2023-02-02 18:18:38 +0900

    JS 1

---
+++ app.js
@@ -0,0 +1,35 @@
+/*
+console.log(5959);
+console.log("chaemin");
+
+const a = 5;
+const b = 2;
+const c = "2";
+let myName = "chaemin";
+
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
+console.log("hello"+myName);
+
+myName = "chaemini";
+
+console.log("your name is " + myName);
+*/
+/*
+const amIFat = null;
+let something;
+console.log(something, amIFat);*/
+
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat"];
+console.log(daysOfWeek);
+
+//get item from array
+//console.log(daysOfWeek[1]);
+
+//add one more day to the array
+daysOfWeek.push("sun");
+console.log(daysOfWeek);
